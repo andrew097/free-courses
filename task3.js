6e90091ddc086d79f82c52d092e7fa3b3b7420bd
Andrey Kozhevnikov <andrew.kozh97@gmail.com>
2018-06-07 11:13:32 +0500

    Добавит склонение количества часов

--- task3.js
+++ task3.js
@@ -8,6 +8,9 @@ var S;
 var hcnt;
 var mincnt;
 var seccnt;
+var hstr;
+var minstr;
+var secstr;
 
 t1 = Number(process.argv[2]);
 t2 = Number(process.argv[3]);
@@ -18,9 +21,29 @@ seccnt = S % 60;
 mincnt = Math.floor(S / 60) % 60;
 hcnt = Math.floor(S / 3600);
 
+switch (hcnt) {
+    case 1:
+        hstr = "час";
+        break;
+
+    case 0:
+    case 5:
+    case 6:
+    case 7:
+        hstr = "часов";
+        break;
+
+    case 2:
+    case 3:
+    case 4:
+        hstr = "часа";
+    default:
+        break;
+}
+
 if (seccnt > 0) {
-    process.stdout.write(hcnt.toString() + ' час ' + mincnt.toString() + ' минут ' + seccnt.toString() + ' секунд');
+    process.stdout.write(hcnt.toString() + ' ' + hstr + ' ' + mincnt.toString() + ' минут ' + seccnt.toString() + ' секунд');
 }
 else {
-    process.stdout.write(hcnt.toString() + ' час ' + mincnt.toString() + ' минут ');
+    process.stdout.write(hcnt.toString() + ' ' +  hstr + ' ' + mincnt.toString() + ' минут ');
 }
